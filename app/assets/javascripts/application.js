e61ef80f7790d8c7df557ab92995132c7bb93474
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-04-03 15:47:05 -0300

    kind of fixes the nav issue but not really

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap/dropdown
 //= require turbolinks
 //= require_tree .
