f0c89dab73dc3cf8c394b95e930de6a7007a4f33
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-04-05 18:48:32 -0300

    adding database diagrams

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :articles, only: [:edit, :update]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
