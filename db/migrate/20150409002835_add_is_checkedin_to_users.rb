44b9132577e31ad0edf1945c70a0ecd981a46793
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-04-08 19:59:22 -0500

    deleted meaning less files

--- db/migrate/20150409002835_add_is_checkedin_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIsCheckedinToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :is_checkedin, :boolean
-  end
-end
--- db/migrate/20150409004152_remove_is_checkedin_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIsCheckedinFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :is_checkedin, :boolean
-  end
-end
