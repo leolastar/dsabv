61f97106c5d1f983dd7e33444ea0b67b6a39da8e
kevinhan66 <rtkevinhan66@gmail.com>
2015-04-19 10:52:47 -0500

    Reverted changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,12 +5,4 @@ class Event < ActiveRecord::Base
   def send_event_register_email
     EventMailer.event_register_notification(self).deliver
   end
-
-  settings index: { number_of_shards: 1} do
-    mappings dynamic: 'false' do
-      indexes :title, analyzer: 'english'
-      indexes :place, analyzer: 'english'
-      indexes :description, analyzer: 'keyword'
-    end
-  end
 end
