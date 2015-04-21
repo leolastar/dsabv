f7a4ae245e70fd460d72cd4020010e8474c01474
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-04-21 11:41:27 -0500

    add step definitions for searching

---
+++ features/step_definitions/search_steps.rb
@@ -0,0 +1,17 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+Then(/^I should not see any results$/) do
+  steps %{
+    Then I should see "No matches. Retry the search or try a new search."
+  }
+end
