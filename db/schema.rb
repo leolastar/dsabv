2da1cd13fb8b6878e404e9ef9f4048f144abec5d
kevinhan66 <rtkevinhan66@gmail.com>
2015-04-29 21:19:34 -0500

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428152403) do
+ActiveRecord::Schema.define(version: 20150430012849) do
 
   create_table "appointments", force: true do |t|
     t.integer  "user_id"
