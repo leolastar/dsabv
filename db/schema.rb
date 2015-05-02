50686bbc23550da63c82c2037112809ef4777d0c
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-05-02 13:08:44 -0500

    add days model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502173046) do
+ActiveRecord::Schema.define(version: 20150502180028) do
 
   create_table "appointments", force: true do |t|
     t.integer  "user_id"
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 20150502173046) do
     t.integer  "total_capacity"
     t.integer  "remaining_capacity"
     t.integer  "ds_class_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "days", force: true do |t|
+    t.string   "day"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
