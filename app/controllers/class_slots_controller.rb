4c398fb6aad7c11f7312640e9874099b66526933
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-05-02 12:54:30 -0500

    change associations

--- app/controllers/class_slots_controller.rb
+++ app/controllers/class_slots_controller.rb
@@ -1,7 +1,41 @@
 class ClassSlotsController < ApplicationController
+  before_action :admin_user, only: [:create, :destroy]
+
   def create
+    event = Event.find params[:event_id]
+    @time_slot = event.time_slots.build(time_slot_params)
+    @time_slot[:start_time].change(year:  event.date.year,
+                                   month: event.date.month,
+                                   day:   event.date.day)
+    @time_slot[:end_time].change(year:  event.date.year,
+                                 month: event.date.month,
+                                 day:   event.date.day)
+    @time_slot[:remaining_capacity] = @time_slot[:total_capacity]
+    if @time_slot.save
+      flash[:success] = "New time slot added."
+      redirect_to event_path(event)
+    else
+      render 'static_pages/home'
+    end
   end
 
-  def destory
+  def destroy
+    event = Event.find params[:event_id]
+    @time_slot = TimeSlot.find params[:id]
+    @time_slot.users.each do |user|
+      EventMailer.event_cancellation(user, @time_slot).deliver
+    end
+    @time_slot.destroy
+    flash[:notice] = "Time slot deleted."
+    redirect_to event_path(event)
   end
+
+  private
+
+    def time_slot_params
+      params.require(:time_slot).permit(:start_time,
+                                        :end_time,
+                                        :total_capacity)
+    end
+end
 end
--- app/models/class_slot.rb
+++ app/models/class_slot.rb
@@ -1,2 +1,17 @@
 class ClassSlot < ActiveRecord::Base
+  has_many :days
+  belongs_to :ds_class
+  validate :ds_class_id, presence: true
+
+# A time slot of an event can be registered by many users
+  has_many :appointments
+  has_many :users, through: :appointments
+
+  default_scope -> { order(start_time: :asc) }
+
+  def capacity_left
+    self.total_capacity - self.users.count
+  end
+end
+
 end
