116c96d4a6ec8db49072d72e8af7a51c3397c53d
Jean Jaures <plaminj@hotmail.com>
2015-05-05 15:05:23 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -99,7 +99,7 @@ def registration_confirmation
 
   def show_registrations
     user = User.find(params[:id])
-    @registrations = user.time_slots.event.order('date ASC, time_slots ASC')
+    @registrations = user.time_slots.event.order('date ASC')
     @class_slots = user.class_slots
   end
 
