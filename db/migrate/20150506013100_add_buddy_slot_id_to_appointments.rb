d527a054ede82578c2878b3bf9514ba7c17586cf
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-05-05 21:40:42 -0500

    delete duplicate file

--- db/migrate/20150506013100_add_buddy_slot_id_to_appointments.rb
+++
@@ -1,5 +0,0 @@
-class AddBuddySlotIdToAppointments < ActiveRecord::Migration
-  def change
-    add_column :appointments, :buddy_slot_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150506013100) do
+ActiveRecord::Schema.define(version: 20150505220955) do
 
   create_table "appointments", force: true do |t|
     t.integer  "user_id"
