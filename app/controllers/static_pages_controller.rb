154f1a3860fbff670c0525a3680a0df8d9929815
chrisdlangham <chrisdlangham@neo.tamu.edu>
2015-05-13 21:16:25 -0500

    calendar now has buddy walk events

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-  	@article_top = Article.find_by_id(1)
-  	@article_bottom = Article.find_by_id(2)
+    @article_top = Article.find_by_id(1)
+    @article_bottom = Article.find_by_id(2)
   end
 
   def calendar
-  	@date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @events_by_date = Event.all.group_by(&:date)
+    @events_by_date.reverse_merge!(BuddyWalk.all.group_by(&:date))
   end
 end
